cf9b4712fb0cc16bf9fc76ec6f26ea53fd69b1b9
Jinita Patel <46331880+jiniep@users.noreply.github.com>
2023-12-12 15:18:33 +1100

    Create genre_rentals_revenue.sql

---
+++ sql_queries/genre_rentals_revenue.sql
@@ -0,0 +1,15 @@
+/*revenue and number of rents for each category */
+SELECT 
+  c.name, 
+  COUNT(DISTINCT f.film_id) as number_of_films, 
+  COUNT(p.rental_id) as number_of_rentals, SUM(p.amount) as revenue, 
+COUNT(DISTINCT r.customer_id) as number_of_customers, MAX(p.amount) max_rent_price, MIN(p.amount) as min_rent_price, 
+avg (p.amount) as avg_price,  AVG(f.rental_rate) as avg_rental_rate, AVG(f.rental_duration) avg_rental_duration
+FROM payment p
+RIGHT JOIN rental r on r.rental_id = p.rental_id
+RIGHT JOIN inventory iv ON iv.inventory_id = r.inventory_id
+RIGHT JOIN film f on f.film_id = iv.film_id
+RIGHT JOIN film_category fc ON fc.film_id = f.film_id
+RIGHT JOIN category c on c.category_id = fc.category_id
+GROUP by c.name
+ORDER by revenue DESC, number_of_rentals DESC
