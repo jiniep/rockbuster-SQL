b37e3951fb615cb6cb5ef283b8429d728493c88d
Jinita Patel <46331880+jiniep@users.noreply.github.com>
2023-12-12 13:47:33 +1100

    Create countries_by_customers_revenue.sql

---
+++ sql_queries/countries_by_customers_revenue.sql
@@ -0,0 +1,17 @@
+/* show all countries rental, customers, films and revenue totals */
+SELECT 
+  c4.country_id, 
+  c4.country, 
+  COUNT(r.rental_id) as number_of_rentals,
+  COUNT(DISTINCT c.customer_id) as number_of_customers,
+  COUNT(iv.film_id) as number_of_films,
+  SUM(p.amount) as total_revenue
+FROM rental r
+INNER JOIN customer c on r.customer_id = c.customer_id
+INNER JOIN address a ON a.address_id = c.address_id
+INNER JOIN city c3 on c3.city_id = a.city_id
+INNER JOIN country c4 on c4.country_id = c3.country_id
+INNER JOIN inventory iv ON iv.inventory_id = r.inventory_id
+INNER JOIN payment p on r.rental_id = p.rental_id
+GROUP BY c4.country_id, c4.country
+ORDER BY total_revenue DESC
